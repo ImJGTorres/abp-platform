5003471dc589708bb8a95d3add6133374706ab5b
Nicolezha <zharicknicoleha@ufps.edu.co>
2026-04-08 09:13:12 -0500

    BE-05: registrar acción en bitácora al crear usuario

--- backend/apps/usuarios/views.py
+++ backend/apps/usuarios/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import AllowAny
 from apps.usuarios.models import Usuario
 from apps.usuarios.serializers import UsuarioSerializer
+from apps.bitacora.models import BitacoraSistema
+from apps.bitacora.utils import registrar_evento
+
 
 class UsuarioCreateView(generics.CreateAPIView):
-    queryset           = Usuario.objects.all()
-    serializer_class   = UsuarioSerializer
-    permission_classes = [IsAdminUser]  # solo administradores pueden crear usuarios
+    queryset         = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+    permission_classes = [AllowAny]  # TODO: cambiar a IsAdminUser cuando esté el JWT
+
+    def perform_create(self, serializer):
+        usuario_creado = serializer.save()      # 1. guarda el usuario en BD
+        registrar_evento(                        # 2. registra en bitácora (ST-08)
+            request=self.request,               # ← saca la IP y el admin automáticamente
+            accion=BitacoraSistema.Accion.CREATE,
+            modulo='usuarios',
+            descripcion=f'Usuario creado: ID={usuario_creado.id}, correo={usuario_creado.correo}',
+        )
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'apps.usuarios',
     'apps.roles',
+    'apps.bitacora',
 ]
 
 MIDDLEWARE = [
