8fbf3e184028fa01f5e37363519c7a532ea00697
dansugp <danielagp@ufps.edu.co>
2026-04-15 18:25:57 -0500

    e2e hu002, avance e2e hu001

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt', 
+    'corsheaders',
     'apps.usuarios',
     'apps.roles',
     'apps.bitacora',
@@ -60,6 +61,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -154,3 +156,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
--- frontend/cypress/e2e/login.cy.js
+++
@@ -1,6 +0,0 @@
-describe('HU-002 - Login', () => {
-  it('muestra la pantalla de login', () => {
-    cy.visit('http://localhost:5173/login')
-    cy.url().should('include', '/login')
-  })
-})
