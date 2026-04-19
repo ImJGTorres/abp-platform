da5255e5b9d3edc2ade9cf0c58d8f0e91d62f4a0
Gabriel Torres <torresgabriel70537@gmail.com>
2026-04-19 18:12:54 -0500

    cambio en settings 2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -184,8 +184,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
-if os.path.exists(os.path.join(BASE_DIR, 'static')):
-    STATICFILES_DIRS.append(os.path.join(BASE_DIR, 'static'))
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
