35df34ecb57adfa0a6df78943212d6f9c64a3ecf
dansugp <danielagp@ufps.edu.co>
2026-04-21 17:54:59 -0500

    diseno HTML en correo de recuperacion y configuracion de permisos DRF

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -61,6 +61,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 # Import timedelta para configurar tiempos de expiración (BE-01)
